b1ec2ae22ddcb57512d210ebf5b814d2dfa84135
neworderofjamie <james.knight@manchester.ac.uk>
2016-11-14 15:09:23 +0000

    Configurations to build convolution layers with two sizes of kernel

--- runtime/conv_layer/build/1x1/config.h
+++ runtime/conv_layer/build/1x1/config.h
@@ -3,12 +3,18 @@
 // Rig CPP common
 #include "rig_cpp_common/circular_buffer.h"
 
+// Conv layer includes
+#include "../../conv_kernel.h"
+#include "../../input.h"
+#include "../../neurons.h"
+
 namespace ConvLayer
 {
 //-----------------------------------------------------------------------------
 // Typedefines
 //-----------------------------------------------------------------------------
 typedef CircularBuffer<uint32_t, 256> SpikeInputBuffer;
-typedef ConvKernelBase<int8_t, 3> ConvKernel;
+typedef ConvKernelBase<int8_t, 1> ConvKernel;
+typedef InputBase<int8_t> Input:
 typedef NeuronsBase<int16_t> Neurons;
-};
+}
