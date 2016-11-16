b7a4e44232a788f5209dafe30a6a0acad0e309b4
neworderofjamie <james.knight@manchester.ac.uk>
2016-11-16 10:35:24 +0000

    Stride is a compile-time parameter

---
+++ runtime/conv_layer/build/3x3_1/config.h
@@ -0,0 +1,20 @@
+#pragma once
+
+// Rig CPP common
+#include "rig_cpp_common/circular_buffer.h"
+
+// Conv layer includes
+#include "../../conv_kernel.h"
+#include "../../input.h"
+#include "../../neurons.h"
+
+namespace ConvLayer
+{
+//-----------------------------------------------------------------------------
+// Typedefines
+//-----------------------------------------------------------------------------
+typedef Common::CircularBuffer<uint32_t, 256> SpikeInputBuffer;
+typedef ConvKernelBase<int8_t, 3, 1> ConvKernel;
+typedef InputBase<int8_t> Input;
+typedef NeuronsBase<int16_t> Neurons;
+}
